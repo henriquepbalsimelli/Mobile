841c96e136a684f1fcfd8691a5b851790ed77c16
henriquepedrozo <henrique.pedrozo@shopper.com.br>
2022-08-09 07:48:57 -0300

    feat: linka ação de apertar o botão com uma funçãozinha de imprimir o input no log

--- app/src/main/java/com/example/firstappkotlin/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/com/example/firstappkotlin/ui/activity/FormularioProdutoActivity.kt
@@ -1,8 +1,23 @@
 package com.example.firstappkotlin.ui.activity
 
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.example.firstappkotlin.R
 
 class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario_produto) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
+        val botaoSalvar = findViewById<Button>(R.id.botao_salvar)
+        botaoSalvar.setOnClickListener {
+            val campoNome = findViewById<EditText>(R.id.nome)
+            val nome = campoNome.text.toString()
+            Log.i("FormularioProduto", "Oncreate: $nome")
+        }
+    }
 }
