632287a3bda1c5a1feec7de9b93322620b87ccc3
henriquepedrozo <henrique.pedrozo@shopper.com.br>
2022-08-12 07:40:19 -0300

    feat: termina de refatorar e deixa projeto aberto para melhorias

--- app/src/main/java/com/example/firstappkotlin/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/com/example/firstappkotlin/ui/activity/FormularioProdutoActivity.kt
@@ -15,27 +15,12 @@ class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario
         super.onCreate(savedInstanceState)
 
         val botaoSalvar = findViewById<Button>(R.id.botao_salvar)//processo de binding
+        configuraBotaoSalvar(botaoSalvar)
+    }
+
+    private fun configuraBotaoSalvar(botaoSalvar: Button) {
         botaoSalvar.setOnClickListener {
-            val campoNome = findViewById<EditText>(R.id.nome)
-            val nome = campoNome.text.toString()
-
-            val campoDescricao = findViewById<EditText>(R.id.descricao)//processo de binding
-            val descricao = campoDescricao.text.toString()
-
-            val campoValor = findViewById<EditText>(R.id.valor) //processo de binding
-            val valorEmTexto = campoValor.text.toString()
-
-            val valor = if(valorEmTexto.isBlank()){
-                BigDecimal.ZERO
-            }else{
-                BigDecimal(valorEmTexto)
-            }
-
-            val produtoNovo = Produto(
-                nome = nome,
-                descricao = descricao,
-                valor = valor
-            )
+            val produtoNovo = criaProduto()
 
             Log.i("FormularioProduto", "Oncreate: $produtoNovo")
             val dao = ProdutosDao()
@@ -44,5 +29,28 @@ class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario
             Log.i("FormularioProduto", "Oncreate: ${dao.buscaTodos()}")
             finish()
         }
+    }
+
+    private fun criaProduto(): Produto {
+        val campoNome = findViewById<EditText>(R.id.produto_item_nome)
+        val nome = campoNome.text.toString()
+
+        val campoDescricao = findViewById<EditText>(R.id.produto_item_descricao)//processo de binding
+        val descricao = campoDescricao.text.toString()
+
+        val campoValor = findViewById<EditText>(R.id.produto_item_valor) //processo de binding
+        val valorEmTexto = campoValor.text.toString()
+
+        val valor = if (valorEmTexto.isBlank()) {
+            BigDecimal.ZERO
+        } else {
+            BigDecimal(valorEmTexto)
+        }
+
+        return Produto(
+            nome = nome,
+            descricao = descricao,
+            valor = valor
+        )
     }
 }
